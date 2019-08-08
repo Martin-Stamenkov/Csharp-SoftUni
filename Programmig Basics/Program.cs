33da84159acdfc15683d956c24211fe163b0644c
martinnstamenkov@gmail.com <martinnstamenkov@gmail.com>
2019-08-08 16:31:14 +0300

    first steps in coding

---
+++ Programmig Basics/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaBSimleOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sideA = int.Parse(Console.ReadLine());
+            int result = sideA * sideA;
+            Console.WriteLine(result);
+
+        }
+    }
+}
