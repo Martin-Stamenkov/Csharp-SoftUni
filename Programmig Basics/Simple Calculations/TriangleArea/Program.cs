23d81990a680d36c845ca0339b914986abe7462e
martinnstamenkov@gmail.com <martinnstamenkov@gmail.com>
2019-08-08 16:42:56 +0300

    First steps in coding

---
+++ Programmig Basics/Simple Calculations/TriangleArea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double side = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double area = side * height / 2;
+            Console.WriteLine("{0:f2}",area);
+        }
+    }
+}
