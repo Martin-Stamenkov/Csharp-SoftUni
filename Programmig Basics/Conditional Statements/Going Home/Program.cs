8766016be002784933b2d7dd11f4ebb7b1866072
martinnstamenkov@gmail.com <martinnstamenkov@gmail.com>
2019-08-10 16:38:36 +0300

    First step in coding

---
+++ Programmig Basics/Conditional Statements/Going Home/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Going_Home
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double distanceKm = double.Parse(Console.ReadLine());
+            double petrolFor100km = double.Parse(Console.ReadLine());
+            double pricePetrol = double.Parse(Console.ReadLine());
+            double moneyWins = double.Parse(Console.ReadLine());
+
+            double carPetrolCost = distanceKm * petrolFor100km / 100;
+            double totalCost = carPetrolCost * pricePetrol;
+            if (totalCost > moneyWins)
+            {
+                Console.WriteLine($"Sorry, you cannot go home. Each will receive {(moneyWins / 5):f2} money.");
+            }
+            else if (totalCost<=moneyWins)
+            {
+                Console.WriteLine($"You can go home. {moneyWins-totalCost:f2} money left.");
+            }
+
+        }
+    }
+}
