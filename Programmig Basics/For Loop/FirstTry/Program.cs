8eff79f897e4defcc680e51bc44c25b1a00709b5
martinnstamenkov@gmail.com <martinnstamenkov@gmail.com>
2019-08-10 16:41:38 +0300

    first steps in coding

---
+++ Programmig Basics/For Loop/FirstTry/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int computers = int.Parse(Console.ReadLine());
+            double sales = 0;
+            double sumRating = 0;
+
+            for (int i = 0; i < computers; i++)
+            {
+                int r = int.Parse(Console.ReadLine());
+                int rating = r % 10;
+                double possibleSales = r / 10;
+                sumRating += rating;
+
+                if (rating == 2)
+                {
+                    sales += 0;
+                }
+                else if (rating == 3)
+                {
+                    sales += possibleSales * 0.5;
+                }
+                else if (rating == 4)
+                {
+                    sales += possibleSales * 0.7;
+                }
+                else if (rating == 5)
+                {
+                    sales += possibleSales * 0.85;
+                }
+                else if (rating == 6)
+                {
+                    sales += possibleSales;
+                }
+            }
+
+            double avgRating = sumRating / computers;
+
+            Console.WriteLine($"{sales:f2}");
+            Console.WriteLine($"{avgRating:f2}");
+
+        }
+    }
+}
+        
---
+++ Programmig Basics/For Loop/Project Prize/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Prize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int projectSteps = int.Parse(Console.ReadLine());
+            double prizeForOnePoint = double.Parse(Console.ReadLine());
+            int totalPoints = 0;
+
+            for (int i = 1; i <= projectSteps; i++)
+            {
+                int points = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    totalPoints += (points * 2);
+                }
+                else
+                {
+                    totalPoints += points;
+                }
+            }
+            double totalPrize = prizeForOnePoint * totalPoints;
+            Console.WriteLine($"The project prize was {totalPrize:f2} lv.");
+        }
+    }
+}
