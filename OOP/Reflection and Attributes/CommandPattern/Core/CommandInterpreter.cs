bd0ccc053819434f9bf30f696e32fb83eee8cf57
martinnstamenkov@gmail.com <martinnstamenkov@gmail.com>
2019-11-22 19:19:45 +0200

    Add Command Pattern

---
+++ OOP/Reflection and Attributes/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,38 @@
+﻿using CommandPattern.Commands;
+using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core
+{ 
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] inputArgs = args.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+            string[] commandArgs = inputArgs.Skip(1).ToArray();
+
+            string commandName = (inputArgs[0] + "Command").ToLower();
+
+            var commandType = Assembly.GetCallingAssembly().GetTypes().FirstOrDefault(x=>x.Name.ToLower() == commandName);
+
+            if (commandType == null)
+            {
+                throw new Exception("Invalid command type!");
+            }
+
+            var instance = Activator.CreateInstance(commandType) as ICommand;
+
+            if (instance == null)
+            {
+                throw new Exception("Invalid instance!");
+            }
+
+            string result = instance.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
