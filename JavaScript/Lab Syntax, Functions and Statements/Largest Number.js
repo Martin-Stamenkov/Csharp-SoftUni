af726cf166231003c12207769c7344eda78c95ae
martinnstamenkov@gmail.com <martinnstamenkov@gmail.com>
2020-01-14 12:06:55 +0200

    add

--- JavaScript/Lab Syntax, Functions and Statements/Largest Number.js
+++ JavaScript/Lab Syntax, Functions and Statements/Largest Number.js
@@ -10,6 +10,6 @@ function solve(num1,num2,num3){
     else if(num3 > num1 && num3 > num2){
         result = num3;
     }
-    console.log('The largest number is {result}.')
+    console.log('The largest number is ${result}.')
 }
 solve(1,2,3)
