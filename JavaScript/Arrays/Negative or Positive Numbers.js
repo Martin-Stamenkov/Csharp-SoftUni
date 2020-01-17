74f6c19ff3bd4d61b89ce8a23de20b9c58d6e0a4
martinnstamenkov@gmail.com <martinnstamenkov@gmail.com>
2020-01-17 09:23:25 +0200

    Add solution

--- JavaScript/Arrays/Negative or Positive Numbers.js
+++ JavaScript/Arrays/Negative or Positive Numbers.js
@@ -3,14 +3,14 @@ function solve(input) {
     let myArray = input.map(Number);
     let result = [];
 
-  for (const element of myArray) {
-      if (element<0) {
-          result.unshift(element);
+    for (const element of myArray) {
+        if (element < 0) {
+            result.unshift(element);
 
-      } else {
-          result.push(element)
-      }
-  }
+        } else {
+            result.push(element)
+        }
+    }
     result.forEach(element => console.log(element))
 }
 //solve([3, -2, 0, -1])
