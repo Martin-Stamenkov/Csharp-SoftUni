23d1d5b2507a672b385ff53a91eff8f2e0dd50e9
martinnstamenkov@gmail.com <martinnstamenkov@gmail.com>
2020-01-17 10:14:28 +0200

    ....

--- JavaScript/Arrays/Print an Array with a Given Delimiter.js
+++ JavaScript/Arrays/Print an Array with a Given Delimiter.js
@@ -3,10 +3,4 @@ function solve(input){
     let delimiter = input.pop();
     console.log(input.join(delimiter));
 }
-solve(['How about no?', 
-'I',
-'will', 
-'not', 
-'do', 
-'it!', 
-'_'])
+solve(['How about no?', 'I','will', 'not', 'do', 'it!', '_']);
