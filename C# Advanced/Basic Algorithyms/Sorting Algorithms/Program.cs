2bfbd2bdab676c08d82c7f7e85420cb1902c6eb8
martinnstamenkov@gmail.com <martinnstamenkov@gmail.com>
2020-01-19 13:20:15 +0200

    Add Sorting Algorithms

---
+++ C# Advanced/Basic Algorithyms/Sorting Algorithms/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Sorting_Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 4, 1, 3, 2, 4 , 2 , 8, -50 , 11 , - 10 };
+            int[] array1 = { 4, 1, 3, 2, 10, -2, -6, 6 };
+
+            SelectionSort(array);
+            BubbleSort(array1);
+
+            Console.WriteLine(string.Join(" ", array));
+            Console.WriteLine(string.Join(" ", array1));
+        }
+
+        private static void BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] < array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+
+        private static void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[minIndex] > array[j])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
+}
