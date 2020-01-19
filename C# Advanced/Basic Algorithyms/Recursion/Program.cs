9e6887cfa679019a3fe58b627de1ca909aaacd85
martinnstamenkov@gmail.com <martinnstamenkov@gmail.com>
2020-01-19 11:20:38 +0200

    Add Basic Algorithms

---
+++ C# Advanced/Basic Algorithyms/Recursion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Recursion
+{
+    public class Program
+    {
+        private static void Recursion(int a)
+        {
+            a += 1;
+
+            Console.WriteLine(a);
+            if (a == 10)
+            {
+                return;
+            }
+
+            Recursion(a);
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 5;
+
+            Recursion(a);
+        }
+    }
+}
---
+++ C# Advanced/Basic Algorithyms/Sum Array with Recursion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sum_Array_with_Recursion
+{
+    public class Program
+    {
+        private static int SumArray(int[] myArray, int index)
+        {
+            if (index == myArray.Length)
+            {
+                return 0;
+            }
+            return myArray[index] + SumArray(myArray, index + 1);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 3, 4, 5 };
+
+            int index = 0;
+
+            int result = SumArray(myArray, index);
+
+            Console.WriteLine(result);
+        }
+    }
+}
