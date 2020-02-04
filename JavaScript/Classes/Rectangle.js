056dbd852242b8795d0f0c918695970198dce0e0
martinnstamenkov@gmail.com <martinnstamenkov@gmail.com>
2020-02-04 17:10:19 +0200

    Add Rectangle solution

---
+++ JavaScript/Classes/Rectangle.js
@@ -0,0 +1,14 @@
+class Rectangle {
+
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    calcArea() {
+        return this.height * this.width
+    }
+}
+let rect = new Rectangle(4,5,'red')
+console.log(rect.calcArea())
