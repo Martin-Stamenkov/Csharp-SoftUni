4db899c0c1de7594d7969dc3d481fceb98855512
martinnstamenkov@gmail.com <martinnstamenkov@gmail.com>
2020-02-06 13:01:39 +0200

    Unity.js

--- JavaScript/Classes/Unity.js
+++ JavaScript/Classes/Unity.js
@@ -25,13 +25,11 @@ class Rat {
 let firstRat = new Rat("Peter");
 console.log(firstRat.toString()); // Peter
 console.log(firstRat.getRats()); // []
-
 firstRat.unite(new Rat("George"));
 firstRat.unite(new Rat("Alex"));
 console.log(firstRat.getRats());
 // [ Rat { name: 'George', unitedRats: [] },
 //  Rat { name: 'Alex', unitedRats: [] } ]
-
 console.log(firstRat.toString());
 // Peter
 // ##George
