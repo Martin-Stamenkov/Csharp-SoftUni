2a54731a3810ae052bc14648998e35ce3e39f754
martinnstamenkov@gmail.com <martinnstamenkov@gmail.com>
2020-02-22 16:01:52 +0200

    Add Exam 26 October

--- JavaScript/Exam Preparation/JS Advanced Retake Exam - 10 December 2019/Christmas Dinner/Christmas Dinner.js
+++ JavaScript/Exam Preparation/JS Advanced Retake Exam - 10 December 2019/Christmas Dinner/Christmas Dinner.js
@@ -66,30 +66,30 @@ class ChristmasDinner {
     }
 
 }
-let dinner = new ChristmasDinner(300);
-console.log(dinner.shopping(['Salt', 1]));
-dinner.shopping(['Beans', 3]);
-dinner.shopping(['Cabbage', 4]);
-dinner.shopping(['Rice', 2]);
-dinner.shopping(['Savory', 1]);
-dinner.shopping(['Peppers', 1]);
-dinner.shopping(['Fruits', 40]);
-dinner.shopping(['Honey', 10]);
-dinner.recipes({
-    recipeName: 'Oshav',
-    productsList: ['Fruits', 'Honey']
-});
-dinner.recipes({
-    recipeName: 'Folded cabbage leaves filled with rice',
-    productsList: ['Cabbage', 'Rice', 'Salt', 'Savory']
-});
-dinner.recipes({
-    recipeName: 'Peppers filled with beans',
-    productsList: ['Beans', 'Peppers', 'Salt']
-});
-dinner.inviteGuests('Ivan', 'Oshav');
-dinner.inviteGuests('Petar', 'Folded cabbage leaves filled with rice');
-dinner.inviteGuests('Georgi', 'Peppers filled with beans');
-console.log(dinner.showAttendance());
+// let dinner = new ChristmasDinner(300);
+// console.log(dinner.shopping(['Salt', 1]));
+// dinner.shopping(['Beans', 3]);
+// dinner.shopping(['Cabbage', 4]);
+// dinner.shopping(['Rice', 2]);
+// dinner.shopping(['Savory', 1]);
+// dinner.shopping(['Peppers', 1]);
+// dinner.shopping(['Fruits', 40]);
+// dinner.shopping(['Honey', 10]);
+// dinner.recipes({
+//     recipeName: 'Oshav',
+//     productsList: ['Fruits', 'Honey']
+// });
+// dinner.recipes({
+//     recipeName: 'Folded cabbage leaves filled with rice',
+//     productsList: ['Cabbage', 'Rice', 'Salt', 'Savory']
+// });
+// dinner.recipes({
+//     recipeName: 'Peppers filled with beans',
+//     productsList: ['Beans', 'Peppers', 'Salt']
+// });
+// dinner.inviteGuests('Ivan', 'Oshav');
+// dinner.inviteGuests('Petar', 'Folded cabbage leaves filled with rice');
+// dinner.inviteGuests('Georgi', 'Peppers filled with beans');
+// console.log(dinner.showAttendance());
 
 
