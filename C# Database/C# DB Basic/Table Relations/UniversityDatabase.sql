b697d25b6f96065270552ff91e2acdc7c11f01fc
martinnstamenkov@gmail.com <martinnstamenkov@gmail.com>
2020-06-06 13:31:55 +0300

    Upload: UniversityDatabase relations

---
+++ C# Database/C# DB Basic/Table Relations/UniversityDatabase.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE University 
+
+USE University
+
+CREATE TABLE Majors 
+(
+	MajorID INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+)
+
+CREATE TABLE Students 
+(
+	StudentID INT PRIMARY KEY IDENTITY,
+	StudentNumber CHAR(8) NOT NULL,
+	StudentName VARCHAR(20) NOT NULL,
+	MajorID INT FOREIGN KEY REFERENCES  Majors(MajorID) NOT NULL,
+)
+
+CREATE TABLE Payments 
+(
+	PaymentsID INT PRIMARY KEY IDENTITY,
+	PaymentDate DATE NOT NULL,
+	PaymentAmount DECIMAL(6,2) NOT NULL,
+	StudentID INT FOREIGN KEY REFERENCES  Students(StudentID) NOT NULL,
+)
+
+CREATE TABLE Subjects 
+(
+	SubjectID INT PRIMARY KEY IDENTITY,
+	SubjectName VARCHAR(50) NOT NULL,
+)
+
+CREATE TABLE Agenda 
+(
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID) NOT NULL,
+	SubjectID INT FOREIGN KEY REFERENCES Subjects(SubjectID) NOT NULL,
+	PRIMARY KEY(StudentID, SubjectID)
+)
